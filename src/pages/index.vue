d8028307f6e4d69c6e4aea46ded83833674053bc
yxl <1013551253@qq.com>
2019-01-15 19:04:41 +0800

    定位

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,9 +3,34 @@
 </template>
 
 <script>
-
+import { Toast } from 'vant'
 export default{
-    
+
+    data() {
+        return {
+            lat: '',
+            lag: ''
+        }
+    },
+
+    created() {
+        this.getLocation()
+    },
+
+    methods: {
+        getLocation(){ 
+            if (window.navigator.geolocation){ 
+                window.navigator.geolocation.getCurrentPosition((position)=>{
+                    var lat = position.coords.latitude; //纬度 
+                    var lag = position.coords.longitude; //经度 
+                    this.lat = lat,
+                    this.lag = lag
+                })
+            }else{ 
+                Toast('浏览器不支持地理定位') 
+            } 
+        },
+    }
 }
 </script>
 
